0cdf6604d37a3cdd437b69a029eece9cd6843647
steng234 <stefan.anghel787@gmail.com>
2023-12-04 22:12:23 +0100

    si

--- src/main/java/client/Client_backend.java
+++ src/main/java/client/Client_backend.java
@@ -1,4 +1,23 @@
 package client;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
 
-public class Client_backend {
+import java.util.Objects;
+
+public class Client_backend extends Application{
+
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("io.fxml")));
+        primaryStage.setTitle("FXML Example");
+        primaryStage.setScene(new Scene(root, 300, 200));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
