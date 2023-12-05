ff4cc16989355d3fbe11d983f45968e49f41fb8a
ilaila05 <zorzellailaria1ci@gmail.com>
2023-12-05 17:55:59 +0100

    Changes by air

--- src/main/java/client/Client_backend.java
+++ src/main/java/client/Client_backend.java
@@ -2,6 +2,7 @@ package client;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,6 @@
 module mnsChat {
     requires javafx.fxml;
     requires javafx.controls;
-    opens client to javafx.graphics;
+    opens client to javafx.fxml;
     exports client;
 }
