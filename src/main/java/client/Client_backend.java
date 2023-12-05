c6f61f226d2fd1a5559dc5c018063727b0b078c9
ilaila05 <zorzellailaria1ci@gmail.com>
2023-12-05 17:55:51 +0100

    creazione calcolatrice (base progetto)

--- src/main/java/client/Client_backend.java
+++ src/main/java/client/Client_backend.java
@@ -5,19 +5,42 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 
-import java.util.Objects;
-
 public class Client_backend extends Application{
+    private boolean loaded = false;
+    @Override
+    public void start(Stage primaryStage) {
+        try{
+            /*URL resourceUrl = getClass().getClassLoader().getResource("client/io.fxml");
 
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("io.fxml")));
-        primaryStage.setTitle("FXML Example");
-        primaryStage.setScene(new Scene(root, 300, 200));
-        primaryStage.show();
+            if (resourceUrl == null) {
+                System.out.println("FXML resource not found");
+            }*/
+
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("/io.fxml"));
+
+
+            Parent root = fxmlLoader.load();
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.setResizable(false);
+            primaryStage.show();
+
+        } catch (NullPointerException e){
+            e.printStackTrace();
+            System.out.println("NULLPOINTER EXCEPTION: " + e.getMessage());
+        }
+        catch (IllegalStateException e){
+            e.printStackTrace();
+            System.out.println("ILLEGALSTATE EXCEPTION: " + e.getMessage());
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            System.out.println("OTHER EXCEPTION: " + e.getMessage());
+        }
     }
 
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(args);
     }
-
 }
