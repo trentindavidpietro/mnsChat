73dff4029c910903df575ad3d0a6cd8d1c04715c
ilaila05 <zorzellailaria1ci@gmail.com>
2023-12-09 09:40:39 +0100

    base di inizio

--- src/main/java/client/Client_frontend.java
+++ src/main/java/client/Client_frontend.java
@@ -1,76 +1,47 @@
 package client;
 
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-import java.io.IOException;
 public class Client_frontend extends Application{
+    private boolean loaded = false;
+    @Override
+    public void start(Stage primaryStage) {
+        try{
+            /*URL resourceUrl = getClass().getClassLoader().getResource("client/esempio_io.fxml");
 
-    public static void main(String[] args) {
+            if (resourceUrl == null) {
+                System.out.println("FXML resource not found");
+            }*/
+
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("/esempio_io.fxml"));
 
 
-        Application.launch(args);
+            Parent root = fxmlLoader.load();
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.setResizable(false);
+            primaryStage.show();
+
+        } catch (NullPointerException e){
+            e.printStackTrace();
+            System.out.println("NULLPOINTER EXCEPTION: " + e.getMessage());
+        }
+        catch (IllegalStateException e){
+            e.printStackTrace();
+            System.out.println("ILLEGALSTATE EXCEPTION: " + e.getMessage());
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            System.out.println("OTHER EXCEPTION: " + e.getMessage());
+        }
     }
 
-    @Override
-    public void start(Stage stage)
-    {
-        Label namelbl=new Label("Inserisci nome");
-        TextField nameFld=new TextField();
-
-        Label msg=new Label();
-        msg.setStyle("-fx-text-fill: blue;");
-
-        //creo i bottoni
-        Button sayHelloBtn=new Button("Dì ciao");
-        Button exitBtn=new Button("Exit");
-        exitBtn.setOnAction(e-> Platform.exit());
-
-        sayHelloBtn.setOnAction(e->
-                {
-                    String name=nameFld.getText();
-                    if(name.trim().length()>0)
-                    {
-                        msg.setText("Ciao "+name);
-                    }
-                    else {
-                        msg.setText("ciao");
-                    }
-                }
-
-        );
-
-
-
-
-        // Create a VBox node
-        VBox root=new VBox();
-        root.setSpacing(5);
-
-
-        // Add the Text node to the VBox as a child node
-        root.getChildren().addAll(namelbl,nameFld,msg,sayHelloBtn,exitBtn);
-
-        // Create a scene
-        //The following statement creates a scene with the VBox as the root node, with 300px
-        //width and 50px height:
-        Scene scene=new Scene(root,500,500);
-
-        stage.setScene(scene);
-
-        //titolo dello stage
-        stage.setTitle("ciao");
-
-        //mostro lo stage
-        stage.show();
+    public static void main(String[] args) {
+        Application.launch(args);
     }
 }
